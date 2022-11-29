cf82ef54b0e96359833adc0cffe0ee60b1293906
Thomas <thomaspierson32@gmail.com>
2022-11-29 10:19:20 +0100

    add css to main css

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -8,18 +8,15 @@ const data = {
 </script>
 
 <template>
-    <div class='container'>
-        <div>
-            <h1>Coop-slack</h1>
-            <h2>Login</h2>
-            <form type="login">
-                <label>Mail</label>
-                <input type="email" placeholder="Email" v-model="data.email">
-                <label>Password</label>
-                <input type="password" placeholder="password" v-model="data.password">
-                <button @click="user.setConnected(data.email, data.password)">Login</button>
-            </form>
-            <a href="/register">Register</a>
-        </div>
+    <div class='login-view'>
+        <h1>Login</h1>
+        <form type="login">
+            <label>Mail</label>
+            <input type="email" placeholder="Email" v-model="data.email">
+            <label>Password</label>
+            <input type="password" placeholder="password" v-model="data.password">
+            <button @click="user.setConnected(data.email, data.password)">Login</button>
+        </form>
+        <a href="/register">Register</a>
     </div>
 </template>
