3a21ac8cb532446bd2d321254289a21f4cd815a8
anthod57 <87667439+anthod57@users.noreply.github.com>
2022-11-29 10:42:17 +0100

    register done

--- src/stores/user.js
+++ src/stores/user.js
@@ -3,14 +3,20 @@ import { defineStore } from 'pinia'
 
 export const useUserStore = defineStore('user', () => {
   const isConnected = ref(false);
+  const user = ref(null);
 
   function setConnected() {
     isConnected.value = true;
-  }  
+  }
+
   function disconnect() {
     isConnected.value = false;
   }
-  return { isConnected, setConnected, disconnect }
+
+  function setUser(_user) {
+    user.value = _user;
+  }
+  return { isConnected, setConnected, disconnect, setUser }
 }, {
   persist: true,
 })
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,21 +1,38 @@
 <script setup>
+	import router from "@/router";
+
 	const user = useUserStore();
 
-	if (user.isConnected) {
-	}
-	onMounted(() => {});
+	const data = reactive({ email: "", fullname: "", password: "" });
+	const passwordConfirmation = ref("");
+
+	const Register = () => {
+		if (data.password !== passwordConfirmation.value || data.email == "" || data.fullname == "") {
+			return;
+		}
+		api
+			.post("members", { body: data })
+			.then((e) => {
+				router.push("login");
+			})
+			.catch((e) => {
+				console.error(e);
+			});
+
+		console.log(user.user);
+	};
 </script>
 
 <template>
-	<div class="about">
+	<div class="register-view">
 		<h1>Register</h1>
 
-		<form action="" id="register">
-			<input type="text" name="fullname" id="fullname" placeholder="Full Name" required />
-			<input type="email" name="email" id="email" placeholder="Email Address" required />
-			<input type="password" name="password" id="password" placeholder="Password" required />
-			<input type="password" name="password2" id="password2" placeholder="Password confirmation" required />
-			<button>Créer un compte</button>
+		<form id="register" onsubmit="return false">
+			<input v-model="data.fullname" type="text" name="fullname" id="fullname" placeholder="Full Name" required />
+			<input v-model="data.email" type="email" name="email" id="email" placeholder="Email Address" required />
+			<input :class="data.password != passwordConfirmation ? 'wrong-password' : ''" v-model="data.password" type="password" name="password" id="password" placeholder="Password" required />
+			<input :class="data.password != passwordConfirmation ? 'wrong-password' : ''" v-model="passwordConfirmation" type="password" name="password2" id="password2" placeholder="Password confirmation" required />
+			<button @click="Register">Créer un compte</button>
 		</form>
 	</div>
 </template>
@@ -23,5 +40,9 @@
 <style scoped>
 	h1 {
 		text-align: center;
+	}
+
+	.wrong-password {
+		border: 1px rgba(176, 13, 13, 0.799) solid;
 	}
 </style>
