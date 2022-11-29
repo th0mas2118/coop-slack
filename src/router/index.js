7c96df1c2444f59226f80822a4216b97213753b8
anthod57 <87667439+anthod57@users.noreply.github.com>
2022-11-29 10:11:17 +0100

    style and rotuer

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import { useUserStore } from '../stores/user';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,6 +19,14 @@ const router = createRouter({
       path: '/register',
       name: 'register',
       component: () => import('../views/RegisterView.vue'),
+      beforeEnter: (to, from, next) => {
+        const user = useUserStore();
+
+        if (user.isConnected) {
+          next('home');
+        }
+        next();
+      },
     },
     {
       path: '/about',
---
+++ src/views/RegisterView.vue
@@ -0,0 +1,27 @@
+<script setup>
+	const user = useUserStore();
+
+	if (user.isConnected) {
+	}
+	onMounted(() => {});
+</script>
+
+<template>
+	<div class="about">
+		<h1>Register</h1>
+
+		<form action="" id="register">
+			<input type="text" name="fullname" id="fullname" placeholder="Full Name" required />
+			<input type="email" name="email" id="email" placeholder="Email Address" required />
+			<input type="password" name="password" id="password" placeholder="Password" required />
+			<input type="password" name="password2" id="password2" placeholder="Password confirmation" required />
+			<button>Créer un compte</button>
+		</form>
+	</div>
+</template>
+
+<style scoped>
+	h1 {
+		text-align: center;
+	}
+</style>
