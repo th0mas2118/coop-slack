238f27e914ade28ca5a979a6e73edaadd8d82ced
Thomas <thomaspierson32@gmail.com>
2022-12-05 15:03:31 +0100

    fix package-json

--- src/views/ConversationListView.vue
+++ src/views/ConversationListView.vue
@@ -3,6 +3,9 @@
 import { useUserStore } from '@/stores/user'
 const user = useUserStore()
 const conversations = []
+if (!user.isConnected) {
+    router.push('login')
+}
 await api.get(`channels?token=${user.member.token}`)
 console.log(conversations)
 </script>
