1f9dc292b536a643600d16b837266309d184d351
anthod57 <87667439+anthod57@users.noreply.github.com>
2022-12-05 14:00:10 +0100

    removed thewelcome

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,9 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
-  <main>
-    <TheWelcome />
-    <a href="/register">Register</a>
-    <a href="/login">Login</a>
-  </main>
+	<main>
+		<TheWelcome />
+		<a href="/register">Register</a>
+		<a href="/login">Login</a>
+	</main>
 </template>
