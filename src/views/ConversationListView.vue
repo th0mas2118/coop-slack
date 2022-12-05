b6bf6e2097f7ba887b7a6134a6d81c1820dd4718
Thomas <thomaspierson32@gmail.com>
2022-12-05 14:56:54 +0100

    affichage conversation

--- src/views/ConversationListView.vue
+++ src/views/ConversationListView.vue
@@ -1,7 +1,12 @@
+<!-- FAIT PAR PIERSON THOMAS -->
 <script setup>
-
+import { useUserStore } from '@/stores/user'
+const user = useUserStore()
+const conversations = []
 </script>
 
 <template>
-
+    <div v-for="conversation in conversations">
+        {{ conversation }}
+    </div>
 </template>
