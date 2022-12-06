8f1b900ac099ad9091b98b181e90a8e439c679f0
Thomas <thomaspierson32@gmail.com>
2022-12-06 08:49:21 +0100

    fix conversation list messsage

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,6 +1,18 @@
 <template lang="">
-    <div>
-        <h2>{{message}}</h2>
+    <div class="message">
+        <div class="avatar">
+            <img :src="'https://gravatar.com/avatar/' + msg['member_id'] + '?s=200&d=robohash&r=x'" alt="" />
+        </div>
+        <section>
+            <header>
+                <div>
+                    <b><RouterLink :to="{ name: 'member', params: { id: msg['member_id'] } }">{{msg['member_name']}}</RouterLink></b>
+                    -
+                    <time>{{msg['date']}}</time>
+                </div>
+            </header>
+            <div class="content">{{message.message}}</div>
+        </section>
     </div>
 </template>
 <script setup>
@@ -9,8 +21,31 @@ const members = useMembersStore();
 const props = defineProps({
     message: {},
 });
-// console.log(message)
+let msg = []
+members.members.forEach(element => {
+    if (element.id === props.message.member_id) {
+        console.log(element)
+        msg['member_name'] = element.fullname
+        msg['member_id'] = element.id
+        msg['date'] = element.created_at
+    }
+});
 </script>
-<style lang="" scoped>
-    
+<style lang="scss" scoped>
+.message {
+    display: flex;
+    flex-direction: row
+}
+
+.avatar {
+    img {
+        width: 50px;
+        height: 50px;
+    }
+}
+
+header {
+    display: flex;
+    flex-direction: row;
+}
 </style>
